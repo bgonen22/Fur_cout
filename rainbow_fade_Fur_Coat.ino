09495d45fe3ac61ad5446ae99b1c72f69d0dd46c
bgonen22 <bgonen22@gmail.com>
2017-06-05 22:55:35 +0300

    shooting star

--- rainbow_fade_Fur_Coat.ino
+++ rainbow_fade_Fur_Coat.ino
@@ -3,6 +3,10 @@
   #include <avr/power.h>
 #endif
 
+// shootingStar:
+#define STAR_LENGTH 5 // how many pixels the star will be
+#define STAR_DELAY_TIME 10
+
 #define PINL 7
 #define PINR 9
 #define PINM 8
@@ -15,7 +19,9 @@
 #define NUMPIXELS3      21
 
 #define NUMPIXELS_M     27
-#define POWER 20 // the base power of the leds
+#define POWER 45 // the base power of the leds
+
+#define NUM_OF_COLORS  12
 
 #define UNCONNECTED_ANALOG_PIN 0// for random seed
 // Parameter 1 = number of pixels in strip
@@ -36,7 +42,7 @@ Adafruit_NeoPixel pixels3 = Adafruit_NeoPixel(NUMPIXELS_M, PINM, NEO_GRB + NEO_K
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
-volatile int interrupt_flag=0;
+volatile bool state = 0;
 int max_pix;
 /////////////////////////////////////////////////////////////////
 void setup() {
@@ -51,32 +57,109 @@ void setup() {
    pinMode(PinInt, INPUT_PULLUP);
    attachInterrupt(digitalPinToInterrupt(PinInt), OnInterupt, FALLING); 
 }
-bool state = 0;
+
 /////////////////////////////////////////////////////////////////
 void loop() {
 /////////////////////////////////////////////////////////////////
 //  Serial.print("interrupt_flag ");
 //  Serial.println(interrupt_flag);
-  if (interrupt_flag) {    
-    state = !state;
-    cli();
-    interrupt_flag = 0;
-    sei();
-  }
-  
+    
 //  Serial.print("state ");
 //  Serial.println(state);
   if (state == 0) {
-    startGradiant(1,0,20,200); // gradiant without powered led startGradiant(int color_jump, int power_jump,int delayval, int num_of_cycles)
+    startGradiant(1,0,100,1); // gradiant without powered led startGradiant(int color_jump, int power_jump,int delayval,bool one_direction)
   } else {
-    startWings(60,500,17); //startWings(int delay_time, int time_left, int num_of_colors)
+    //startWings(60,500,17); //startWings(int delay_time, int time_left, int num_of_colors)
+    shootingStar();
   }
 }
+
+/////////////////////////////////////////////////////////////////
+void shootingStar() {
+/////////////////////////////////////////////////////////////////
+  while (1) {
+    if (state != 1) {
+      return;    
+    }
+    long line = random(7);
+    long color = random(NUM_OF_COLORS);
+    shootStar(line,color);
+  }
+  
+}
+
+/////////////////////////////////////////////////////////////////
+void shootStar(long line, long color) {
+/////////////////////////////////////////////////////////////////
+   int start_pixel,end_pixel,strip;
+   switch (line) {
+    // set the start and end pixels that will participate
+    case 0:
+      start_pixel = 0;
+      end_pixel = NUMPIXELS-1;
+      strip = 0;
+      break;
+    case 1:
+      start_pixel = NUMPIXELS;
+      end_pixel = NUMPIXELS+NUMPIXELS2-1;
+      strip = 0;
+      break;
+    case 2:
+      start_pixel = NUMPIXELS+NUMPIXELS2;
+      end_pixel = start_pixel+NUMPIXELS3;
+      strip = 0;
+      break;
+    case 3:
+      start_pixel = 0;
+      end_pixel = NUMPIXELS-1;
+      strip = 1;
+      break;
+    case 4:
+      start_pixel = NUMPIXELS;
+      end_pixel = NUMPIXELS+NUMPIXELS2-1;
+      strip = 1;
+      break;
+    case 5:
+      start_pixel = NUMPIXELS+NUMPIXELS2;
+      end_pixel = start_pixel+NUMPIXELS3;
+      strip = 1;
+      break;   
+    case 6:
+      start_pixel = 0;
+      end_pixel = NUMPIXELS_M;
+      strip = 3;
+      break;   
+   }
+   lightStar(start_pixel,end_pixel,strip,color);
+}
+
+/////////////////////////////////////////////////////////////////
+void lightStar(int start_pixel, int end_pixel, int strip, int color) {
+/////////////////////////////////////////////////////////////////
+  for (int i = start_pixel ; i <= end_pixel; ++i) {
+    addPixel(i,getColor(color,POWER),strip);
+    if (i - STAR_LENGTH >= start_pixel) {
+      addPixel(i-STAR_LENGTH,0,strip);
+    }
+    showPixels();
+    delay(STAR_DELAY_TIME);
+  }
+  int power = POWER;
+  for (int i = end_pixel - STAR_LENGTH; i <= end_pixel; ++i) {
+    addPixel(i,0,strip);
+    for (int j = i+1 ; j <= end_pixel; ++j) {
+      addPixel(j,getColor(color,power/2),strip);
+    }
+    showPixels();
+    delay(STAR_DELAY_TIME);
+  }
+}
+
 /////////////////////////////////////////////////////////////////
 void OnInterupt() {
 /////////////////////////////////////////////////////////////////
   cli();
-  interrupt_flag = 1;
+  state = !state;  
   sei();  
 }
 
@@ -95,12 +178,12 @@ void startWings(int delay_time, int time_left, int num_of_colors) {
    int j=0;
    clearAll();   
    while (time_left > 0) {   
-    if (interrupt_flag) {    
+    if (state != 1) {    
       return;
     }  
-    setWingLine(getColor(j,POWER,num_of_colors),NUMPIXELS,0);
-    setWingLine(getColor(j+1,POWER,num_of_colors),NUMPIXELS2,NUMPIXELS);
-    setWingLine(getColor(j+2,POWER,num_of_colors),NUMPIXELS3,NUMPIXELS+NUMPIXELS2);
+    setWingLine(getColor(j,POWER),NUMPIXELS,0);
+    setWingLine(getColor(j+1,POWER),NUMPIXELS2,NUMPIXELS);
+    setWingLine(getColor(j+2,POWER),NUMPIXELS3,NUMPIXELS+NUMPIXELS2);
     delay(delay_time);
     time_left -= delay_time;
     showPixels();
@@ -129,26 +212,26 @@ void setWingLine(uint32_t color, int num_of_pixels, int start_pixel) {
 // color_jump - each iteration the color will move this amount of pixels
 // power_jump - each iteration the led with more power will move this amount of pixels
 // delayval - the delay between to iterations
-// num_of_cycel - number of cycles that the function will run
+// one_direction - go only up or up-down
 /////////////////////////////////////////////////////////////////
-void startGradiant(int color_jump, int power_jump,int delayval, int num_of_cycles) {
+void startGradiant(int color_jump, int power_jump,int delayval, bool one_direction) {
 /////////////////////////////////////////////////////////////////
   int color_i =0;
   int power_i = 0;
   int color_add =color_jump;
   int power_add =power_jump;
-  for (int a =1 ; a<2*max_pix*num_of_cycles;a++) { // number of cycles it will run 
-    if (interrupt_flag) {    
+  while (1) {  
+    if (state != 0) {    
       return;
     }
 //    Serial.println(power_i);
     //    Serial.println(max_pix);
     color_i = color_i +color_add;
     power_i = power_i + power_add;
-    if (color_i >= max_pix) {
-      //  color_add = -color_jump;
-    } else if (color_i <= 0) {
-      //  color_add = color_jump;
+    if (!one_direction && color_i >= max_pix) {
+        color_add = -color_jump;
+    } else if (!one_direction && color_i <= 0) {
+        color_add = color_jump;
     }
     if (power_i > max_pix) {
       power_add = -power_jump;
@@ -156,13 +239,8 @@ void startGradiant(int color_jump, int power_jump,int delayval, int num_of_cycle
       power_add = power_jump;
     }
     gradiant(color_i,power_i,0,0);
-    int delay_val;
-    if (a%max_pix >= max_pix-5) {
-      delay_val = 1.5*delayval;
-    } else {
-      delay_val = delayval;
-    }
-    delay(delay_val); // Delay for a period of time (in milliseconds).
+    
+    delay(delayval); // Delay for a period of time (in milliseconds).
 //    Serial.print(power_i);
 //    Serial.print(" ");
 //    Serial.println(color_i);
@@ -173,7 +251,7 @@ void startGradiant(int color_jump, int power_jump,int delayval, int num_of_cycle
 //   color_i - color changing from the index base color (for color movement)
 //   power_i - the led index to be whith more power
 //   strip - 0 - both strips; 1 - strip 1; 2 - strip 2
-//   line  - 0 - all the lines ; 1 - line 1; 2 - line 2; 3- line 3 
+//   line  - 0 - all the lines ; 1 - line 1; 2 - line 2; 3- line 3 (ecery strip has 3 lines: up-down-up
 /////////////////////////////////////////////////////////////////
 void gradiant(int color_i,int power_i,int strip,int line) { 
 /////////////////////////////////////////////////////////////////  
@@ -193,20 +271,20 @@ void gradiant(int color_i,int power_i,int strip,int line) {
 
     // FIRST LINE
     if (i<NUMPIXELS && line == 1 || line == 0) {
-      addPixel(i, getColor(i+color_i,power_tmp,max_pix),strip);
+      addPixel(i, getColor(i+color_i,power_tmp),strip);
     }
  
     // SECOND LINE
     int index= NUMPIXELS+NUMPIXELS2-1-i;
     if (index>=NUMPIXELS && line == 2 || line == 0) {
-    addPixel(index, getColor(i+color_i,power_tmp,max_pix),strip);
+    addPixel(index, getColor(i+color_i,power_tmp),strip);
     }
  
 
     // THIRD LINE   
     if (line == 3 || line == 0) {
       index= NUMPIXELS+NUMPIXELS2+i;
-      addPixel(index, getColor(i+color_i,power_tmp,max_pix),strip);
+      addPixel(index, getColor(i+color_i,power_tmp),strip);
     } 
   }
   showPixels();   
@@ -214,23 +292,23 @@ void gradiant(int color_i,int power_i,int strip,int line) {
 }
 
 /////////////////////////////////////////////////////////////////
-uint32_t getColor(int i, int power, int max_pix) {
+uint32_t getColor(int i, int power) {
 /////////////////////////////////////////////////////////////////
-  i = i%max_pix;
-  if (i<max_pix/3) {
-       int p = i*3*power/max_pix;
+  i = i%NUM_OF_COLORS;  
+  if (i<NUM_OF_COLORS/3) {
+       int p = i*3*power/NUM_OF_COLORS;
 //         Serial.print(p);         
 //         Serial.println(power);                               
        return pixels.Color(power-p,p,0);
-  } else if (i <2*max_pix/3) {
-      // int p = i*3*power/(2*max_pix);
-       int p = (i-max_pix/3)*3*power/max_pix;
+  } else if (i <2*NUM_OF_COLORS/3) {
+      // int p = i*3*power/(2*NUM_OF_COLORS);
+       int p = (i-NUM_OF_COLORS/3)*3*power/NUM_OF_COLORS;
        //Serial.println(power);
        //Serial.println(p);
        return pixels.Color(0,power-p,p);
   } else {
-       //int p = i*power/max_pix;
-       int p = (i-2*max_pix/3)*3*power/max_pix;
+       //int p = i*power/NUM_OF_COLORS;
+       int p = (i-2*NUM_OF_COLORS/3)*3*power/NUM_OF_COLORS;
        return pixels.Color(p,0,power-p); 
   }
 }
